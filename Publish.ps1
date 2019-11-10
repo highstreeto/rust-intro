848668b23c459b30793495572e47cd9b680a2237
Martin Hochstrasser <highstreeto@users.noreply.github.com>
2019-11-10 19:40:35 +0100

    Update Publish

--- Publish.ps1
+++ Publish.ps1
@@ -1,3 +1,14 @@
+$ErrorActionPreference = "Stop"
+
+$gitInfo = Get-GitInfo
+$gitLastCommit = $gitInfo.Tip
+
+if ("master" -ne $gitInfo.Branch) {
+    Write-Error "Must be on master branch to release!"
+}
+
+Write-Output "Building for packaging ..."
+
 Push-Location .\slides
 
 npm run package
@@ -5,5 +16,25 @@ Move-Item rust-intro.zip ..\
 
 Pop-Location
 
+Write-Output "Done!"
+
+Write-Output "Stashing and switching to gh-pages ..."
+git stash
+git checkout gh-pages
+Write-Output "Done!"
+
+Write-Output "Expanding build artifact ..."
 Expand-Archive -Path rust-intro.zip -DestinationPath .
 Remove-Item rust-intro.zip
+Write-Output "Done!"
+
+Write-Output "Commiting and pushing (based on $gitLastCommit) ..."
+git add *
+git commit --all -m "Update gh-pages to commit $gitLastCommit"
+git push
+Write-Output "Done!"
+
+Write-Output "Switching back to master and popping stash ..."
+git checkout master
+git stash pop
+Write-Output "Done!"
