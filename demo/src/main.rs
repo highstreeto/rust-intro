82fc830df9e0b7efbc979f4ef19657c181e83936
Martin Hochstrasser <highstreeto@users.noreply.github.com>
2019-11-10 19:26:30 +0100

    Finish last TODOs and add font awesome
    For GitHub icon

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let float: f32 = 1.0;
     let double: f64 = -15.103E3;
     let b: bool = true;
-    let c: char = "🦀";
+    let c: char = '🦀';
     let i = 15; // type is inferred
     // these are all immutable
 
@@ -13,6 +13,7 @@ fn main() {
     k += 1;
     println!("k: {}", k);
 
+    // if
     let input = String::from("Hello World");
     if input == "Hello World" {
         println!("Classic")
@@ -20,6 +21,7 @@ fn main() {
         println!("Unexpected");
     }
 
+    // loop
     let mut counter = 1;
     let loop_ret = loop {
         counter += 1;
@@ -34,12 +36,53 @@ fn main() {
         break
     };
     println!("loop_ret: {:?}", loop_ret);
+
+    // while
+    let mut counter = 1;
+    let while_ret = while counter < 15 {
+        counter += 1;
+    };
+    assert_eq!(counter, 15);
+    assert_eq!(while_ret, ());
+
+    // for
+    let range = (1..101);
+    for elem in range {
+        print!("{} ", elem);
+    }
+
+    // Tuples and Arrays
+    let tuple = (1, true, "Test");
+    let array = [1, 2, 3, 4];
+    let slice = &array[0..2];
+
+    assert_eq!(tuple.1, true);
+    assert_eq!(array[1], 2);
+    assert_eq!(array.len(), 4);
+    assert_eq!(slice.len(), 2);
+
+    // Borrows
+    // 2 mutable borrows
+    // let mut s = String::from("hello");
+
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+    
+    // println!("{}, {}", r1, r2);
+
+    // Mutable borrow with immutable
+    // let mut s = String::from("hello");
+
+    // let r1 = &mut s;
+    // let r2 = &s;
+    
+    // println!("{}, {}", r1, r2);
 }
 
 enum Message {
     Skipped,
-    Ok(u32),
-    Error { code: u32, msg: String },
+    Ok(u32), // tuple
+    Error { code: u32, msg: String }, // struct
 }
 
 impl Message {
@@ -57,4 +100,10 @@ impl Message {
             Message::Error { code, .. } => Some(code)
         }
     }
+}
+
+struct MessageQueue {
+    name: String,
+    messages: Vec<Message>,
+    history: Vec<Message>
 }
